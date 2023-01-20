e292aeb6da23653d4f4018db66ec5ec7e38ba5f8
hyunsoloh <77195736+hyunsoloh@users.noreply.github.com>
2023-01-20 14:08:51 +0900

    Add files via upload

--- ex02_0109.java
+++ ex02_0109.java
@@ -8,6 +8,8 @@ public class ex02_0109 {
         System.out.println("일한시간을 입력하세요: ");
         int time = sc.nextInt();
         int money = 5000;
+        
+        //조건문 이용
 
         // if(time>8){
         //     double pay = money*8 + money*(time-8)*1.5;
@@ -16,6 +18,9 @@ public class ex02_0109 {
         //     double pay = money * time;
         //     System.out.println("총 임금은"+(int)pay+"원 입니다.");
         // }
+
+        //삼항연산자
+
         double pay = time>8?money*8+ money*(time-8)*1.5: money * time;
         System.out.println("총 임금은"+pay+"원 입니다.");
         sc.close();
